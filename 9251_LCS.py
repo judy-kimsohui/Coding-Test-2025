cd82d7d31e807c2d693c5b7b254a46088f2f0114
sodaakim <sohuikim2020@naver.com>
2025-10-10 12:34:22 +0900

    LCS 기록

--- 9251_LCS.py
+++ 9251_LCS.py
@@ -7,6 +7,8 @@ N = len(CS1L)-1
 K = len(CS2L)-1
 
 dp = [[0] * (K+1) for _ in range(N+1)]
+# 첫 번째 문자열의 i번째 문자까지, 두 번째 문자열의 j번째 문자까지 고려했을 때  
+#            만들 수 있는 LCS(최장 공통 부분 수열)의 길이
 
 for i in range(1, N+1):
     for j in range(1, K+1):
