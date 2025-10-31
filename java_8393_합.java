8179d4ed924e8bf84f9f1e3353c2e4b431eda24b
sodaakim <sohuikim2020@naver.com>
2025-10-31 11:20:47 +0900

    java_8393_합

---
+++ java_8393_합.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class java_8393_합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N * (N + 1) / 2);
+    }
+}
